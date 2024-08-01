6e5c168072d3f5fcf4b13a72e96c5ad9529bc5c2
ElDiablo994 <dusan.radic@forwardslashny.com>
2024-08-01 10:18:58 +0200

    Theme commit

--- wp-content/themes/infuse-theme/footer.php
+++ wp-content/themes/infuse-theme/footer.php
@@ -13,9 +13,6 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-<<<<<<< Updated upstream
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'infuse-theme' ) ); ?>">
-=======
 			<hr>
 			<div class="footer_custom-menu">
 				<div class="footer_menu-item">
@@ -29,17 +26,14 @@
 				</div>
 			</div>
 			<div class="footer_copyright-text">
->>>>>>> Stashed changes
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'infuse-theme' ), 'WordPress' );
+				// Get the current year dynamically
+				$current_year = date('Y');
+
+				// Display the dynamic year
+				echo sprintf('© %1$s OurCompany | All Rights Reserved', $current_year);
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'infuse-theme' ), 'infuse-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/infuse-theme/header.php
+++ wp-content/themes/infuse-theme/header.php
@@ -26,45 +26,8 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'infuse-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$infuse_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $infuse_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $infuse_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'infuse-theme' ); ?></button>
-<<<<<<< Updated upstream
-			<div class="contactus-menu">
-              <span class="contactus-text"><span>Our Clients</span></span>
-              <span class="contactus-text02"><span>Our Model</span></span>
-              <span class="contactus-text04"><span>Our Audience</span></span>
-              <div class="contactus-insights">
-                <span class="contactus-text06"><span>Insights</span></span>
-                <span class="contactus-text08"></span>
-              </div>
-              <div class="contactus-group39618">
-                <span class="contactus-text09"></span>
-                <span class="contactus-text10"><span>What We Do</span></span>
-              </div>
-              <div class="contactus-group39601">
-                <span class="contactus-text12"><span>Our Company</span></span>
-                <span class="contactus-text14"></span>
-              </div>
-=======
 			<div class="contactus-menu_desktop">
 				<div class="inner-contactus-menu">
 					<span class="inner_menu-item contactus-text contacus-menu_item">
@@ -123,7 +86,6 @@
 						</button>
 					</div>
 				</div>
->>>>>>> Stashed changes
             </div>
 			<div class="contactus-menu_mobile">
 				<div class="contactus-menu-mail">
